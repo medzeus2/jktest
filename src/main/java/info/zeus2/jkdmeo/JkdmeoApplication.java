1376f3d81288a94aff7c29aed03c2ea402facc33
zeus2 <medzeus2@qq.com>
2019-03-13 22:45:45 +0800

    add files

---
+++ src/main/java/info/zeus2/jkdmeo/JkdmeoApplication.java
@@ -0,0 +1,13 @@
+package info.zeus2.jkdmeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkdmeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JkdmeoApplication.class, args);
+    }
+
+}
